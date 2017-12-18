a7c17353c7248a4829e16afa9b44af9a5cac3cb8
Simon Venshtain <svensht2@gmail.com>
2017-12-17 18:32:47 -0600

    remove noexecpt to support VS2013

--- include/librealsense2/hpp/rs_types.hpp
+++ include/librealsense2/hpp/rs_types.hpp
@@ -26,14 +26,14 @@
 
 struct rs2_frame_callback
 {
-    virtual void                            on_frame(rs2_frame * f) = 0;
+    virtual void                            on_frame(rs2_frame* f) = 0;
     virtual void                            release() = 0;
     virtual                                 ~rs2_frame_callback() {}
 };
 
 struct rs2_frame_processor_callback
 {
-    virtual void                            on_frame(rs2_frame * f, rs2_source * source) = 0;
+    virtual void                            on_frame(rs2_frame* f, rs2_source* source) = 0;
     virtual void                            release() = 0;
     virtual                                 ~rs2_frame_processor_callback() {}
 };
@@ -47,7 +47,7 @@ struct rs2_notifications_callback
 
 struct rs2_log_callback
 {
-    virtual void                            on_event(rs2_log_severity severity, const char * message) = 0;
+    virtual void                            on_event(rs2_log_severity severity, const char* message) = 0;
     virtual void                            release() = 0;
     virtual                                 ~rs2_log_callback() {}
 };
@@ -68,64 +68,68 @@ struct rs2_playback_status_changed_callback
 
 namespace rs2
 {
-    class error : public std::runtime_error
+class error : public std::runtime_error
+{
+    std::string function, args;
+    rs2_exception_type type;
+public:
+    explicit error(rs2_error* err) : runtime_error(rs2_get_error_message(err))
     {
-        std::string function, args;
-        rs2_exception_type type;
-    public:
-        explicit error(rs2_error* err) : runtime_error(rs2_get_error_message(err))
-        {
-            function = (nullptr != rs2_get_failed_function(err)) ? rs2_get_failed_function(err) : std::string();
-            args = (nullptr != rs2_get_failed_args(err)) ? rs2_get_failed_args(err) : std::string();
-            type = rs2_get_librealsense_exception_type(err);
-            rs2_free_error(err);
-        }
+        function = (nullptr != rs2_get_failed_function(err)) ? rs2_get_failed_function(err) : std::string();
+        args = (nullptr != rs2_get_failed_args(err)) ? rs2_get_failed_args(err) : std::string();
+        type = rs2_get_librealsense_exception_type(err);
+        rs2_free_error(err);
+    }
 
-        explicit error(const std::string& message) : runtime_error(message.c_str())
-        {
-            function = "";
-            args = "";
-            type = RS2_EXCEPTION_TYPE_UNKNOWN;
-        }
+    explicit error(const std::string& message) : runtime_error(message.c_str())
+    {
+        function = "";
+        args = "";
+        type = RS2_EXCEPTION_TYPE_UNKNOWN;
+    }
 
-        const std::string& get_failed_function() const
-        {
-            return function;
-        }
+    const std::string& get_failed_function() const
+    {
+        return function;
+    }
 
-        const std::string& get_failed_args() const
-        {
-            return args;
-        }
+    const std::string& get_failed_args() const
+    {
+        return args;
+    }
 
-        rs2_exception_type get_type() const { return type; }
+    rs2_exception_type get_type() const
+    {
+        return type;
+    }
 
-        static void handle(rs2_error* e);
-    };
+    static void handle(rs2_error* e);
+};
 
-    #define RS2_ERROR_CLASS(name, base) \
+#define RS2_ERROR_CLASS(name, base) \
     class name : public base\
     {\
     public:\
-        explicit name(rs2_error* e) noexcept : base(e) {}\
+        explicit name(rs2_error* e) : base(e) {}\
     }
 
-    RS2_ERROR_CLASS(recoverable_error, error);
-    RS2_ERROR_CLASS(unrecoverable_error, error);
-    RS2_ERROR_CLASS(camera_disconnected_error, unrecoverable_error);
-    RS2_ERROR_CLASS(backend_error, unrecoverable_error);
-    RS2_ERROR_CLASS(device_in_recovery_mode_error, unrecoverable_error);
-    RS2_ERROR_CLASS(invalid_value_error, recoverable_error);
-    RS2_ERROR_CLASS(wrong_api_call_sequence_error, recoverable_error);
-    RS2_ERROR_CLASS(not_implemented_error, recoverable_error);
-    #undef RS2_ERROR_CLASS
+RS2_ERROR_CLASS(recoverable_error, error);
+RS2_ERROR_CLASS(unrecoverable_error, error);
+RS2_ERROR_CLASS(camera_disconnected_error, unrecoverable_error);
+RS2_ERROR_CLASS(backend_error, unrecoverable_error);
+RS2_ERROR_CLASS(device_in_recovery_mode_error, unrecoverable_error);
+RS2_ERROR_CLASS(invalid_value_error, recoverable_error);
+RS2_ERROR_CLASS(wrong_api_call_sequence_error, recoverable_error);
+RS2_ERROR_CLASS(not_implemented_error, recoverable_error);
+#undef RS2_ERROR_CLASS
 
-    inline void error::handle(rs2_error* e)
+inline void error::handle(rs2_error* e)
+{
+    if (e)
     {
-        if (e)
+        auto h = rs2_get_librealsense_exception_type(e);
+        switch (h)
         {
-            auto h = rs2_get_librealsense_exception_type(e);
-            switch (h) {
             case RS2_EXCEPTION_TYPE_CAMERA_DISCONNECTED:
                 throw camera_disconnected_error(e);
             case RS2_EXCEPTION_TYPE_BACKEND:
@@ -140,33 +144,33 @@ namespace rs2
                 throw device_in_recovery_mode_error(e);
             default:
                 throw error(e);
-            }
         }
     }
+}
 
-    class context;
-    class device;
-    class device_list;
-    class syncer;
-    class device_base;
-    class roi_sensor;
-    class frame;
+class context;
+class device;
+class device_list;
+class syncer;
+class device_base;
+class roi_sensor;
+class frame;
 
-    struct option_range
-    {
-        float min;
-        float max;
-        float def;
-        float step;
-    };
+struct option_range
+{
+    float min;
+    float max;
+    float def;
+    float step;
+};
 
-    struct region_of_interest
-    {
-        int min_x;
-        int min_y;
-        int max_x;
-        int max_y;
-    };
+struct region_of_interest
+{
+    int min_x;
+    int min_y;
+    int max_x;
+    int max_y;
+};
 }
 
 #endif // LIBREALSENSE_RS2_TYPES_HPP
